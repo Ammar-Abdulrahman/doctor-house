23831e9049c249e829039b329d3380a3725b94fd
Ammar abdulrahman <Ammarabdulrahmman@gmail.com>
2024-05-19 21:50:28 +0300

    feat: update the app after add the tostify

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { RouterProvider } from "react-router-dom";
-import { Suspense } from 'react'
+import { Suspense } from "react";
 import router from "@Routes/AppRoutes.routes";
 import { QueryClient, QueryClientProvider } from "react-query";
 import AppLoader from "@Components/Loader/AppLoader";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useTranslation } from "react-i18next";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const { i18n } = useTranslation();
   return (
     <Suspense fallback={<AppLoader />}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ToastContainer
+          direction={i18n.language === "ar" ? "rtl" : "ltr"}
+          position="bottom-right"
+        />
       </QueryClientProvider>
     </Suspense>
   );
