5999d1fb015b34c86631edba080c9b7e1e636dc7
Ammar abdulrahman <Ammarabdulrahmman@gmail.com>
2024-08-24 12:38:10 +0300

    feat: add animation to side bar list

--- src/Core/Layout/Sidebar/List/index.tsx
+++ src/Core/Layout/Sidebar/List/index.tsx
@@ -1,32 +1,40 @@
 import { List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import theme from "@Styles/theme";
 import { menuItems } from "../items/items";
-import { Link , useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { hasPermission } from "@Utils/index";
 import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
 
 const ListItems = () => {
   const { t, i18n } = useTranslation();
   const location = useLocation();
+
   return (
-    <>
-      <List
-        style={{
-          color: theme.palette.secondary.light,
-          // marginLeft: i18n.language === "en" ? theme.spacing(2) : "",
-          // marginRight: i18n.language === "ar" ? theme.spacing(2) : "",
-          textAlign: i18n.language === "ar" ? "right" : "left",
-          marginTop: "18px",
-          justifyContent: "flex-start",
-          direction: i18n.language === "ar" ? "rtl" : "ltr",
-        }}
-      >
-        {menuItems
-          .filter((item) => hasPermission(item.permission))
-          .map((item) => (
+    <List
+      style={{
+        color: theme.palette.secondary.light,
+        textAlign: i18n.language === "ar" ? "right" : "left",
+        marginTop: "18px",
+        justifyContent: "flex-start",
+        direction: i18n.language === "ar" ? "rtl" : "ltr",
+        overflow: "hidden",
+      }}
+    >
+      {menuItems
+        .filter((item) => hasPermission(item.permission))
+        .map((item) => (
+          <motion.div
+            key={item.text}
+            whileHover={{
+              scaleY: 1.2,
+              scaleX: 1.1,
+              color: "#f8e112",
+            }}
+            transition={{ stiffness: 300, stype: "spring" }}
+          >
             <ListItem
               button
-              key={item.text}
               component={Link}
               to={item.link}
               selected={location.pathname === item.link}
@@ -65,59 +73,10 @@ const ListItems = () => {
                 }}
               />
             </ListItem>
-          ))}
-      </List>
-    </>
+          </motion.div>
+        ))}
+    </List>
   );
 };
 
 export default ListItems;
-
-{
-  /* <Divider /> */
-}
-{
-  /* <List
-        style={{
-          color: theme.palette.secondary.light,
-          marginLeft: i18n.language === "en" ? theme.spacing(2) : "",
-          marginRight: i18n.language === "ar" ? theme.spacing(2) : "",
-          textAlign: i18n.language === "ar" ? "right" : "left",
-          marginTop: "150px",
-          justifyContent: "flex-start",
-          direction: i18n.language === "ar" ? "rtl" : "ltr",
-        }}
-      >
-        {menuBottomItems.map((item) => (
-          <ListItem
-            button
-            key={item.text}
-            component={Link}
-            to={item.link}
-            selected={location.pathname === item.link}
-            style={{ direction: i18n.language === "ar" ? "rtl" : "ltr" }} // تأكد من تطبيق الاتجاه على كل عنصر
-          >
-            <ListItemIcon
-              style={{
-                color: theme.palette.secondary.light,
-                minWidth: 0,
-                alignItems: "center",
-                justifyContent: "center",
-                marginRight: i18n.language === "ar" ? theme.spacing(3) : "",
-                marginLeft: i18n.language === "en" ? theme.spacing(3) : "",
-              }}
-            >
-              <item.icon />
-            </ListItemIcon>
-            <ListItemText
-              primary={t(item.text)}
-              style={{
-                textAlign: i18n.language === "ar" ? "right" : "left",
-                marginRight: i18n.language === "ar" ? theme.spacing(4) : "" ,
-                marginLeft: i18n.language === "en" ? theme.spacing(4) : "" ,
-              }}
-            />
-          </ListItem>
-        ))}
-      </List> */
-}
